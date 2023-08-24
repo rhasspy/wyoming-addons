1789209e5c24fc4d38fffd9960077d2e02404543
Eduard Reñé Claramunt <edurenye@hotmail.com>
2023-08-24 23:47:38 +0200

    Add initial GPU support

--- piper/run.sh
+++ piper/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 python3 -m wyoming_piper \
     --piper '/usr/share/piper/piper' \
+    --cuda \
     --uri 'tcp://0.0.0.0:10200' \
     --data-dir /data \
     --download-dir /data "$@"
